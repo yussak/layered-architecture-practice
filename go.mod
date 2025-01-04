12bb5e383c2136a1891199381ab124e661883400
yussak <yusuke.sakuraba0213@gmail.com>
2025-01-05 01:05:46 +0900

    DB初期化処理追加

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module server
 
 go 1.23.4
+
+require github.com/lib/pq v1.10.9
--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"server/db"
+	"server/routes"
+
+	_ "github.com/lib/pq"
+)
 
 func main() {
-	fmt.Printf("Hello, world!\n")
+	db.Init()
+	routes.SetupRoutes()
+
+	fmt.Println("Server running on port :8080")
+	http.ListenAndServe(":8080", nil)
 }
