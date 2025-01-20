b7ccb707e7406947bf36348971c1f9ed9a970412
yussak <yusuke.sakuraba0213@gmail.com>
2025-01-21 08:07:35 +0900

    add test

--- server/application/todo_service_test.go
+++ server/application/todo_service_test.go
@@ -1,1 +1,48 @@
 package application
+
+import (
+	"reflect"
+	"server/domain"
+	"testing"
+)
+
+type MockTodoRepository struct {
+	Called bool
+	Todos  []domain.Todo
+	Error  error
+}
+
+func (m *MockTodoRepository) GetTodos() ([]domain.Todo, error) {
+	m.Called = true
+	return m.Todos, m.Error
+}
+
+func TestTodoServiceImpl_GetTodos_Success(t *testing.T) {
+	// モックリポジトリを作成
+	mockRepo := &MockTodoRepository{
+		Todos: []domain.Todo{
+			{ID: 1, Name: "Test Todo 1"},
+			{ID: 2, Name: "Test Todo 2"},
+		},
+		Error: nil,
+	}
+
+	// サービスにモックを注入
+	TodoService := TodoService(mockRepo)
+
+	// 実行
+	result, err := TodoService.GetTodos()
+
+	// 検証
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	if !reflect.DeepEqual(result, mockRepo.Todos) {
+		t.Errorf("expected %v, got %v", mockRepo.Todos, result)
+	}
+
+	if !mockRepo.Called {
+		t.Error("expected GetTodos to be called, but it was not")
+	}
+}
